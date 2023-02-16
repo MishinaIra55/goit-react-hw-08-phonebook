375eb158408a25af1a0ef119e2ec68f10f568f3c
MishynaIra <gortenzia1987@gmail.com>
2023-02-16 11:55:26 +0200

    route в компоненте app к страницам приложения

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+import { LoginPage } from '../pages/LoginPage';
+
+import { RegisterPage } from '../pages/RegisterPage';
+import { Layout } from './Loyout';
+import { ContactsPage } from '../pages/ContactsPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path= '/' element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/" element={<RegisterPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<ContactsPage />} />
+      </Route>
+    </Routes>
   );
 };
